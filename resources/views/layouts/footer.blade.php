193df207e2f3cea7b6c54fbf145e9e7610c97033
Khaled Mohamed <38916029+KhaledMohamed72@users.noreply.github.com>
2022-10-24 23:06:44 +0200

    pp

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="main-footer">
+<footer class="main-footer pt-0 pb-0">
     <strong>Copyright &copy; {{ now()->year }} <a href="http://it-gates.com">IT-Gates</a>.</strong>
     All rights reserved.
 </footer>
