c2f95836b93f117f3f1213a4aa7cd0a3b5f6c4d1
Khaled Mohamed <38916029+KhaledMohamed72@users.noreply.github.com>
2022-11-27 01:09:48 +0200

    prescriptions

---
+++ app/Http/Controllers/CareCompanyController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CareCompany;
+use Illuminate\Http\Request;
+
+class CareCompanyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\CareCompany  $careCompany
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CareCompany $careCompany)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\CareCompany  $careCompany
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(CareCompany $careCompany)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\CareCompany  $careCompany
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, CareCompany $careCompany)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\CareCompany  $careCompany
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CareCompany $careCompany)
+    {
+        //
+    }
+}
