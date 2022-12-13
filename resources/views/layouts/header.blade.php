02b9431af577ccdb90da2a02ab362ed6c9f45c0d
KhaledMohamed72 <khaledmohamed7298@gmail.com>
2022-12-13 14:33:30 +0200

    prescription 53

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -96,7 +96,7 @@
             @csrf
         </form>
         <!-- Notifications Dropdown Menu -->
-        <li class="nav-item dropdown">
+        {{--<li class="nav-item dropdown">
             <a class="nav-link" data-toggle="dropdown" href="#">
                 <i class="far fa-bell"></i>
                 <span class="badge badge-warning navbar-badge">15</span>
@@ -121,6 +121,6 @@
                 <div class="dropdown-divider"></div>
                 <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
             </div>
-        </li>
+        </li>--}}
     </ul>
 </nav>
